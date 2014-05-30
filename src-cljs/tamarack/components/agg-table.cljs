0630094b34ff637759c353b9e64e356fc3e7ebff
mhallin <mhallin@gmail.com>
2014-05-30 21:27:24 +0200

    Move inner functions to namespace scope in components and pages

--- src-cljs/tamarack/components/agg-table.cljs
+++ src-cljs/tamarack/components/agg-table.cljs
@@ -6,50 +6,52 @@
             [tamarack.xhr :as xhr]
             [tamarack.routes :as routes]))
 
+(defn- fetch-data [app owner agg-type]
+  (let [[from to] (-> app :timeslice :window)
+        url (str (string/join "/"
+                              [ "/explorer-api/v1/applications"
+                                (-> app :current-app :app-id)
+                                "aggregate"
+                                (subs (str agg-type) 1)])
+                 "?from=" (util/inst->iso from)
+                 "&to=" (util/inst->iso to))]
+    (xhr/send-edn {:method :get
+                   :url url
+                   :on-complete
+                   (fn [res]
+                     (om/set-state! owner :data res))})))
+
+(defn- goto-endpoint [app endpoint e]
+  (.preventDefault e)
+  (routes/navigate-to routes/app-endpoint-overview
+                      {:id (-> @app :current-app :app-id)
+                       :endpoint endpoint}))
+
+(defn- render-row [app round-fn unit [endpoint value]]
+  (html
+   [:tr {:key endpoint}
+    [:td [:a
+          {:href "#" :onClick (partial goto-endpoint app endpoint)}
+          endpoint]]
+    [:td.num-col (str (round-fn value) unit)]]))
+
 (defn component [app owner {:keys [agg-type round-fn unit] :as opts}]
-  (letfn [(fetch-data [app]
-            (let [[from to] (-> app :timeslice :window)
-                  url (str (string/join "/"
-                                        [ "/explorer-api/v1/applications"
-                                          (-> app :current-app :app-id)
-                                          "aggregate"
-                                          (subs (str agg-type) 1)])
-                           "?from=" (util/inst->iso from)
-                           "&to=" (util/inst->iso to))]
-              (xhr/send-edn {:method :get
-                             :url url
-                             :on-complete
-                             (fn [res]
-                               (om/set-state! owner :data res))})))
+  (reify
+    om/IDisplayName
+    (display-name [_] "AggregateTable")
 
-          (goto-endpoint [endpoint e]
-            (.preventDefault e)
-            (routes/navigate-to routes/app-endpoint-overview
-                                {:id (-> @app :current-app :app-id)
-                                 :endpoint endpoint}))
+    om/IWillMount
+    (will-mount [_]
+      (fetch-data app owner agg-type))
 
-          (render-row [[endpoint value]]
-            (html
-             [:tr {:key endpoint}
-              [:td [:a
-                    {:href "#" :onClick (partial goto-endpoint endpoint)}
-                    endpoint]]
-              [:td.num-col (str (round-fn value) unit)]]))]
-    (reify
-      om/IDisplayName
-      (display-name [_] "AggregateTable")
+    om/IWillUpdate
+    (will-update [_ next-props next-state]
+      (when-not (= (:timeslice next-props) (:timeslice app))
+        (fetch-data next-props owner agg-type)))
 
-      om/IWillMount
-      (will-mount [_]
-        (fetch-data app))
-
-      om/IWillUpdate
-      (will-update [_ next-props next-state]
-        (when-not (= (:timeslice next-props) (:timeslice app))
-          (fetch-data next-props)))
-
-      om/IRender
-      (render [_]
-        (html
-         [:table.table.table-hover.table-striped.table-condensed
-          [:tbody (map render-row (om/get-state owner :data))]])))))
+    om/IRender
+    (render [_]
+      (html
+       [:table.table.table-hover.table-striped.table-condensed
+        [:tbody (map (partial render-row app round-fn unit)
+                     (om/get-state owner :data))]]))))
--- src-cljs/tamarack/pages/all_apps.cljs
+++ src-cljs/tamarack/pages/all_apps.cljs
@@ -5,35 +5,37 @@
             [tamarack.routes :as routes]
             [tamarack.xhr :as xhr]))
 
+(defn- app-link-clicked [e row]
+  (.preventDefault e)
+  (routes/navigate-to routes/app-dashboard {:id (:name row)}))
+
+(defn- render-row [row]
+  (html
+   [:tr {:key (:name row)}
+    [:td [:a
+          {:href "#" :onClick #(app-link-clicked % row)}
+          (:display-name row)]]]))
+
 (defn page [app owner]
-  (letfn [(app-link-clicked [e row]
-            (.preventDefault e)
-            (routes/navigate-to routes/app-dashboard {:id (:name row)}))
-          (render-row [row]
-            (html
-             [:tr {:key (:name row)}
-              [:td [:a
-                    {:href "#" :onClick #(app-link-clicked % row)}
-                    (:display-name row)]]]))]
-    (reify
-      om/IDisplayName
-      (display-name [_] "AllApps")
+  (reify
+    om/IDisplayName
+    (display-name [_] "AllApps")
 
-      om/IWillMount
-      (will-mount [_]
-        (xhr/send-edn {:method :get
-                       :url "/explorer-api/v1/applications"
-                       :on-complete
-                       (fn [res]
-                         (om/set-state! owner :applications res))}))
+    om/IWillMount
+    (will-mount [_]
+      (xhr/send-edn {:method :get
+                     :url "/explorer-api/v1/applications"
+                     :on-complete
+                     (fn [res]
+                       (om/set-state! owner :applications res))}))
 
-      om/IRender
-      (render [_]
-        (html
-         [:div.row
-          [:div.col-md-8
-           [:table.table.table-hover.table-striped
-            [:thead [:tr [:th "Application"]]]
-            [:tbody
-             (map render-row
-                  (om/get-state owner :applications))]]]])))))
+    om/IRender
+    (render [_]
+      (html
+       [:div.row
+        [:div.col-md-8
+         [:table.table.table-hover.table-striped
+          [:thead [:tr [:th "Application"]]]
+          [:tbody
+           (map render-row
+                (om/get-state owner :applications))]]]]))))
