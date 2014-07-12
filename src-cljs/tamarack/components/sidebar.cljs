57c8ba2d34d71aa28bc0468f5520a54be66c53ed
mhallin <mhallin@gmail.com>
2014-07-12 21:01:07 +0200

    Page-dependent sidebar

--- src-cljs/tamarack/components/sidebar.cljs
+++ src-cljs/tamarack/components/sidebar.cljs
@@ -1,12 +1,19 @@
 (ns tamarack.components.sidebar
   (:require [om.core :as om :include-macros true]
+            [clojure.string :as string]
             [sablono.core :as html :refer-macros [html]]))
 
-(defn all-apps-items []
+(defn- all-apps-items []
   (html
    [:ol
     [:li.active
      [:a {:href "#"} "Applications"]]]))
+
+(defn- app-items [view app]
+  (html
+   [:ol
+    [:li {:class (when (= view :app-dashboard) "active")}
+     [:a {:href (string/join "/" [ "#/applications" (:name app)])} "Dashboard"]]]))
 
 (defn component [app owner]
   (reify
@@ -15,5 +22,10 @@
 
     om/IRender
     (render [_]
-      :all-apps
-      (all-apps-items))))
+      (case (:view app)
+        :all-apps (all-apps-items)
+
+        (:app-dashboard :app-endpoint-overview)
+        (app-items (:view app) (:current-app app))
+        
+        (html [:ol])))))
