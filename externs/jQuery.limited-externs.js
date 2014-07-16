57854c26c1c66e3a6255688e6c3c32ca4ed07cd4
mhallin <mhallin@gmail.com>
2014-07-16 22:56:56 +0200

    Simple externs file for the jQuery functionality needed

---
+++ externs/jQuery.limited-externs.js
@@ -0,0 +1,16 @@
+function jQuery(arg1, arg2) {}
+
+/**
+ * @constructor
+ * @extends {jQuery}
+ * @param {(jQuerySelector|Element|Object|Array.<Element>|jQuery|string|
+ *     function())=} arg1
+ * @param {(Element|jQuery|Document|
+ *     Object.<string, (string|function(!jQuery.event=))>)=} arg2
+ * @return {!jQuery}
+ */
+function $(arg1, arg2) {}
+
+jQuery.prototype.on = function(arg1, arg2, arg3) {};
+
+jQuery.prototype.tab = function(arg1) {};
